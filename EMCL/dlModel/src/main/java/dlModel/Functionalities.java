a33b42e7e381b37536bedca02107cdc2670c0d60
Emira <emira.ziberi@gmail.com>
2017-12-14 14:10:22 +0100

    Commiting the files

---
+++ EMCL/dlModel/src/main/java/dlModel/Functionalities.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package dlModel;
+
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * @author emira
+ *
+ */
+public class Functionalities {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        }
+
+}
